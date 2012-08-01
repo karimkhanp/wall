7d93b64d95be621962db99ecca6ba0c0d8cda969
Tomek <Tomek@ape29.home>
2012-08-01 10:58:48 +0200

    Chat Tabs styled, moved twitter bootstrap to local

---
+++ includes/script_loader.php
@@ -0,0 +1,35 @@
+    <link href="css/wall.css" rel="stylesheet" type="text/css">
+      <!-- Le styles -->
+    <link href="css/assets/css/bootstrap.css" rel="stylesheet">
+    <link href="css/assets/css/bootstrap-responsive.css" rel="stylesheet">
+
+    <link href="css/assets/js/google-code-prettify/prettify.css" rel="stylesheet">
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+    <script type="text/javascript" src="js/jquery.oembed.js"></script>
+    <script type="text/javascript" src="js/wall.js"></script>
+
+   <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
+    <script src="css/assets/js/jquery.js"></script>
+    <script src="css/assets/js/google-code-prettify/prettify.js"></script>
+    <script src="css/assets/js/bootstrap-transition.js"></script>
+    <script src="css/assets/js/bootstrap-alert.js"></script>
+    <script src="css/assets/js/bootstrap-modal.js"></script>
+    <script src="css/assets/js/bootstrap-dropdown.js"></script>
+    <script src="css/assets/js/bootstrap-scrollspy.js"></script>
+    <script src="css/assets/js/bootstrap-tab.js"></script>
+    <script src="css/assets/js/bootstrap-tooltip.js"></script>
+    <script src="css/assets/js/bootstrap-popover.js"></script>
+    <script src="css/assets/js/bootstrap-button.js"></script>
+    <script src="css/assets/js/bootstrap-collapse.js"></script>
+    <script src="css/assets/js/bootstrap-carousel.js"></script>
+    <script src="css/assets/js/bootstrap-typeahead.js"></script>
+    <script src="css/assets/js/application.js"></script>
--- index.php
+++ index.php
@@ -19,70 +19,82 @@ include_once 'session.php';
 $Wall = new Wall_Updates();
 $updatesarray = $Wall->Updates($uid);
 ?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>Version 3.1 modyfied</title>
-<link href="css/wall.css" rel="stylesheet" type="text/css">
-<link rel="stylesheet" href="http://twitter.github.com/bootstrap/1.4.0/bootstrap.min.css">    
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
- <script type="text/javascript" src="js/jquery.oembed.js"></script>
- <script type="text/javascript" src="js/wall.js"></script>
-</head>
+    <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+    <title>Version 3.1 modyfied</title>
+
+        
+       <?php
+       include_once 'includes/script_loader.php';
+       ?>
+    </head>
+    
 <body>
-<div id="wall_container">
-    <div id="updateboxarea">
-
-    <h4>What's up?</h4>
-        <form method="post" action="">
-            <textarea cols="30" rows="4" name="update" id="update" maxlength="200" ></textarea>
-            <br />
-            <input type="submit"  value=" Update "  id="update_button"  class="update_button"/>
-        </form>
+    
+     <ul class="nav nav-tabs" id="myTab">
+        <li class="active">
+            <a href="#home" data-toggle="tab" >Company</a>
+        </li>
+        <li>
+            <a href="#group"  data-toggle="tab"><?php echo $group_name ?></a>
+        </li>
+       
+    </ul>
+     
+    <div class="tab-content">
+        <div class="tab-pane active" id="home">
+            <div id="wall_container">
+                <div id="updateboxarea">
+                    <form method="post" action="">
+                        <textarea cols="30" rows="4" name="update" id="update" maxlength="200" ></textarea>
+                        <br />
+                        <input type="submit"  value=" Update "  id="update_button"  class="update_button"/>
+                    </form>
+                </div>
+                <div id='flashmessage'>
+                    <div id="flash" align="left"  ></div>
+                </div>
+                <div id="content">
+                    <?php include('load_messages.php'); ?>
+                </div>
+            </div>
+        </div>
+        <div class="tab-pane" id="group">
+             <div id="wall_container" class="private">
+                    <div id="updateboxarea">
+                        <form method="post" action="">
+                            <textarea cols="30" rows="4" name="update" id="update" maxlength="200" ></textarea>
+                            <br />
+                            <input type="hidden" id="group_id" name="group_id" value="<?php echo $group_id ?>">
+                            <input type="submit"  value=" Update "  id="update_private_button"  class="update_button"/>
+                        </form>
+                    </div>
+                <div id='flashmessage'>
+                    <div id="flash" align="left"  ></div>
+                </div>
+                <div id="content">
+                <?php
+                    $Wall = new Wall_Updates();
+                    $updatesarray = $Wall->Updates($uid , $group_id);
+                    include('load_messages.php'); 
+                ?>
+                </div>
+            </div>
+        </div>
     </div>
-<div id='flashmessage'>
-<div id="flash" align="left"  ></div>
-</div>
-<div id="content">
-
-<?php include('load_messages.php'); ?>
-
-</div>
+     
+    <script>
+        jQuery(function () {
+            jQuery('#myTab a:last').tab('show');
+        })
+    </script>
+  
     
-
-
-
-</div>
-<div id="wall_container" class="private">
-    <div id="updateboxarea">
-
-    <h4>What's up?</h4>
-        <form method="post" action="">
-            <textarea cols="30" rows="4" name="update" id="update" maxlength="200" ></textarea>
-            <br />
-            <input type="hidden" id="group_id" name="group_id" value="<?php echo $group_id ?>">
-            <input type="submit"  value=" Update "  id="update_private_button"  class="update_button"/>
-        </form>
-    </div>
-<div id='flashmessage'>
-<div id="flash" align="left"  ></div>
-</div>
-<div id="content">
-
-<?php
+   
     
-    
-    $Wall = new Wall_Updates();
-    $updatesarray = $Wall->Updates($uid , $group_id);
-    include('load_messages.php'); 
-?>
-
-</div>
-    
-
-
-
-</div>
+  
 </body>
 </html>
